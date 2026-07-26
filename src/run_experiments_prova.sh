b81c960ba387bff31cd1d70acd3339ed7a0cf7ef
matte-12 <matteo.gidoni@studenti.unipd.it>
2026-07-26 12:56:22 +0200

    fix .sh

--- src/run_experiments_prova.sh
+++ src/run_experiments_prova.sh
@@ -24,10 +24,7 @@ set -uo pipefail
 # ROOT DEL PROGETTO
 # ============================================================
 
-PROJECT_ROOT="$(
-    cd "$(dirname "${BASH_SOURCE[0]}")"
-    pwd
-)"
+PROJECT_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd )"
 
 cd "$PROJECT_ROOT"
 
